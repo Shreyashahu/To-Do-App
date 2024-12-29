71fe9ebb01e66e6b60ff259cb04dcb9ff60c63c8
ShreyaShahu <123928933+Shreyashahu@users.noreply.github.com>
2024-12-29 21:46:18 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -5,6 +5,19 @@ let ul = document.querySelector("ul");
 btn.addEventListener("click", function(){
     let item = document.createElement("li");
     item.innerText = int.value;
+    
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+    li.appendChild(delBtn);
+    
     ul.appendChild(item);
     int.value = "";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let item = event.target.parentElement;
+        item.remove();
+    }     
 });
